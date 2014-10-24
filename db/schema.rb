1576938812ddd205be0a51a734c59a136ab1ead3
marcwright <marc.wright@gmail.com>
2014-10-24 00:04:59 -0400

    converted to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131029093352) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pages", force: true do |t|
     t.string   "url"
     t.datetime "created_at"

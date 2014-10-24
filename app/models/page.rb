2ef7f8305120fcb7f8d80e4e6710e2a9deb5d509
marcwright <marc.wright@gmail.com>
2014-10-23 23:38:50 -0400

    Page model removing space from query interpolation

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,18 +4,10 @@ class Page < ActiveRecord::Base
 		photo ||= "boogermanloop"
 
 		auth = { query: { client_id: 'c77ea186e7a4465a90729e3c4735403a' }}
-		# q = movie
-		# page_limit = 1
-		search_url = "https://api.instagram.com/v1/tags/#{photo}/media/recent"
+				
+		search_url = "https://api.instagram.com/v1/tags/#{photo.delete(" ")}/media/recent"
 		
 		response = HTTParty.get search_url, auth
 
-		# auth = { query: { client_id: 'c77ea186e7a4465a90729e3c4735403a' }}
-		# id = response["data"][0]["images"]["standard_resolution"]["url"]
-		# # id = '770672122'
-		# photo_url = "https://api.instagram.com/v1/tags/#{movie}/media/recent"
-
-		# response = HTTParty.get photo_url, auth
-		
 	end
 end
